ca3302c7630ee2a6f5cf8f442d5624135047a79f
Nast8065 <n9069968806n@yandex.ru>
2025-08-24 21:58:46 +0700

    изменения в файлах проекта

---
+++ data.py
@@ -0,0 +1,11 @@
+from random import randint
+
+class Person:
+    user_name = 'Anastasia'
+    email = 'Shchavinskaya_29@yandex.ru'
+    password = 'zaq1xsw2'
+
+class RandomData:
+    user_name = 'ABC12^$k'
+    email = f'practikum{randint(0, 999)}@gmail.com'
+    password = f'AS{randint(1000, 9999)}#g'
